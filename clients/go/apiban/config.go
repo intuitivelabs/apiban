8f2b6c429f7f10d3385511a779b9c90b3d69dc52
Andrei Pelinescu-Onciul <andrei@iptel.org>
2021-03-26 15:40:45 +0100

    config: default config + simplified config loading
     - simpler config loading
     - more documented options
     -  -log changed to --log or -l
     -  -config changed to --config or -c
     - logging to the file starts now _after_ the config is
     loaded/parsed (before that => stderr)
     - allow starting without config (it can be completely configured
     from the command line)

--- clients/go/apiban/config.go
+++ clients/go/apiban/config.go
@@ -2,7 +2,7 @@ package apiban
 
 import (
 	"encoding/json"
-	"errors"
+	//"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -25,10 +25,10 @@ type Config struct {
 	Apikey  string `long:"APIKEY" description:"api key"`
 	Lkid    string `long:"LKID" description:"lk id"`
 	Version string `long:"VERSION" description:"protocol version"`
-	Url     string `long:"URL" description:"ban list server url"`
-	Chain   string `long:"CHAIN" description:"ipset chain name"`
-	Tick    string `long:"INTERVAL" description:"tick interval"`
-	Full    string `long:"FULL"`
+	Url     string `long:"URL" description:"URL of blacklisted IPs DB"`
+	Chain   string `long:"CHAIN" description:"ipset chain name for matching entries"`
+	Tick    string `long:"INTERVAL" description:"interval for the list refresh"`
+	Full    string `long:"FULL" description:"yes/no - starting from scratch"`
 	// state filename
 	StateFilename string `long:"STATE_FILENAME" description:"filename for keeping the state"`
 	// ttl for the firewall DROP rules
@@ -38,79 +38,120 @@ type Config struct {
 	// encryption key used for anonymization
 	EncryptionKey string `long:"ENCRYPTION_KEY" description:"encryption key as a hex string (password and key must not be set in the same time"`
 
+	LogFilename string       `short:"l" long:"log" description:"log file or - for stdout"`
+	SetCfgFile  func(string) `short:"c" long:"config" description:"config file"`
+
 	// black list ttl translated into seconds
 	blTtl    int
 	filename string
 }
 
-var DefaultConfig = Config{}
+var DefaultConfig = Config{
+	Url:         "https://siem.intuitivelabs.com/api/",
+	Chain:       "BLOCKER",
+	LogFilename: "/var/log/apiban-ipsets.log",
+	Tick:        "60s",
+	Full:        "no",
+}
 
 // global configuration
-var config = &DefaultConfig
+var config = DefaultConfig
 
 func GetConfig() *Config {
-	return config
+	return &config
 }
 
 // LoadConfig attempts to load the APIBAN configuration file from various locations
-func LoadConfig(configFilename string) (*Config, error) {
+func LoadConfig() (*Config, error) {
 	var filenames []string
+	var errCfgFile error
 
-	// If we have a user-specified configuration file, use it preferentially
-	if configFilename != "" {
-		filenames = append(filenames, configFilename)
+	cfg := &config
+
+	cfgFileCnt := 0
+	// set on config file option function
+	cfg.SetCfgFile = func(f string) {
+		cfgFileCnt++
+		if cfgFileCnt > 10 {
+			errCfgFile = fmt.Errorf("too many config files loaded"+
+				" (%d, current %w)", cfgFileCnt, f)
+			return
+		}
+		fmt.Printf("loading config file %q ...\n", f)
+		if err := flags.IniParse(f, cfg); err != nil {
+			errCfgFile = fmt.Errorf("config file %q parsing failed: %w",
+				f, err)
+			return
+		}
+		cfg.filename = f // save current config name
 	}
 
-	// If we can determine the user configuration directory, try there
-	configDir, err := os.UserConfigDir()
-	if err == nil {
-		filenames = append(filenames, fmt.Sprintf("%s/apiban-ipsets/config.json", configDir))
+	// parse command line
+	if _, err := flags.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("command line parsing failed: %w", err)
+	}
+	if errCfgFile != nil {
+		return nil, errCfgFile
 	}
 
-	// Add standard static locations
-	filenames = append(filenames, defaultConfigFilenames[:]...)
+	if cfgFileCnt == 0 {
+		// no config file on command line
+		// If we can determine the user configuration directory, try there
+		configDir, err := os.UserConfigDir()
+		if err == nil {
+			filenames = append(filenames, fmt.Sprintf("%s/apiban-ipsets/config.json", configDir))
+		}
 
-	for _, loc := range filenames {
+		// Add standard static locations
+		filenames = append(filenames, defaultConfigFilenames[:]...)
 
-		cfg := config
-		err := flags.IniParse(loc, cfg)
-		if err != nil {
-			if _, ok := err.(*os.PathError); ok {
-				// file not found
-				continue
+		for _, loc := range filenames {
+
+			err := flags.IniParse(loc, cfg)
+			if err != nil {
+				if _, ok := err.(*os.PathError); ok {
+					// file not found
+					continue
+				}
+				return nil, fmt.Errorf("failed to read configuration"+
+					" from %s: %w", loc, err)
 			}
-			return nil, fmt.Errorf("failed to read configuration from %s: %w", loc, err)
+
+			// Store the location of the config file so that we can update it
+			// later
+			cfg.filename = loc
+			cfgFileCnt++
+			break
 		}
-
-		// Store the location of the config file so that we can update it later
-		cfg.filename = loc
-
-		if len(cfg.BlacklistTtl) == 0 {
-			cfg.blTtl = 0
-		} else if t, err := time.ParseDuration(cfg.BlacklistTtl); err != nil {
-			// translate configuration parameters if needed
-			return nil, fmt.Errorf("failed to read configuration from %s: %w", loc, err)
-		} else if t.Seconds() < 0 {
-			return nil, fmt.Errorf("failed to read configuration from %s: %w", loc, err)
-		} else {
-			cfg.blTtl = int(t.Seconds())
-		}
-
-		if len(cfg.Passphrase) != 0 && len(cfg.EncryptionKey) != 0 {
-			return nil, fmt.Errorf("failed to read configuration from %s: both passphrase and encryption key are provided", loc)
-		}
-		// EncryptionKey must be either empty or contain 32 hex digits
-		if len(cfg.EncryptionKey) != 0 &&
-			len(cfg.EncryptionKey) != (anonymization.EncryptionKeyLen*2) {
-			return nil, fmt.Errorf("failed to read configuration from %s: invalid length for encryption key (when non-empty, encryption key must have a length of 32 hex digits)", loc)
-		}
-
-		fmt.Println("cfg: ", cfg)
-
-		return cfg, nil
+		// allow the no config file case, it could have been configured
+		// completely from command line
+		//  if cfgFileCnt == 0 {
+		//    return nil, errors.New("failed to locate configuration file")
+		//  }
 	}
 
-	return nil, errors.New("failed to locate configuration file")
+	loc := cfg.filename
+	// translate configuration parameters if needed
+	if t, err := time.ParseDuration(cfg.BlacklistTtl); err != nil {
+		return nil, fmt.Errorf("bad blacklist ttl in %q: %w", loc, err)
+	} else if t.Seconds() < 0 {
+		return nil, fmt.Errorf("blacklist ttl value too small  in %q: %w",
+			loc, err)
+	} else {
+		cfg.blTtl = int(t.Seconds())
+	}
+
+	if len(cfg.Passphrase) != 0 && len(cfg.EncryptionKey) != 0 {
+		return nil, fmt.Errorf("failed to read configuration from %s: both passphrase and encryption key are provided", loc)
+	}
+	// EncryptionKey must be either empty or contain 32 hex digits
+	if len(cfg.EncryptionKey) != 0 &&
+		len(cfg.EncryptionKey) != (anonymization.EncryptionKeyLen*2) {
+		return nil, fmt.Errorf("failed to read configuration from %s: invalid length for encryption key (when non-empty, encryption key must have a length of 32 hex digits)", loc)
+	}
+
+	return cfg, nil
+
 }
 
 // String converts the configuration data structure into a valid JSON string
