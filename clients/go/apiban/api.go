48bbb38b4aa96e02846a5083e2c11d303ccdb13d
Cristian Constantin <cristian@intuitivelabs.com>
2021-09-30 11:06:16 +0200

    moves JSONMap methods next to JSONMap declaration

--- clients/go/apiban/api.go
+++ clients/go/apiban/api.go
@@ -46,6 +46,49 @@ var (
 
 type JSONMap map[string]interface{}
 
+func (metadata JSONMap) Ttl() (ttl int, err error) {
+	ttl = 0
+	err = nil
+
+	if metaTtl, ok := metadata["defaultBlacklistTtl"]; ok {
+		floatTtl, _ := metaTtl.(float64)
+		ttl = int(floatTtl)
+		if ttl < 0 {
+			ttl = 0
+		}
+		return
+	}
+	err = ErrJsonMetadataDefaultBlacklistTtlMissing
+	return
+}
+
+func (metadata JSONMap) Timestamp() (timestamp int, err error) {
+	timestamp = 0
+	err = nil
+
+	if metaTimestamp, ok := metadata["lastTimestamp"]; ok {
+		floatTimestamp, _ := metaTimestamp.(float64)
+		timestamp = int(floatTimestamp)
+		if timestamp < 0 {
+			timestamp = 0
+		}
+		return
+	}
+	err = ErrJsonMetadataGeneratedatMissing
+	return
+}
+
+func (metadata JSONMap) NextEncodedKey() *string {
+	if metaKey, ok := metadata["nextEncodedKey"]; ok {
+		key, _ := metaKey.(string)
+		if len(key) == 0 {
+			return nil
+		}
+		return &key
+	}
+	return nil
+}
+
 // Resource is a generic term for IP addressses, URIs; this interface should be implemented by all resources
 type Resource interface {
 	// Process locally the resource received from the server (by applying b/w listing, fw rules aso)
@@ -263,49 +306,6 @@ func (msg *ErrResponse) Process(api *Api) error {
 	log.Printf(`received an error response for api "%s": %d %s`, api.Path, msg.StatusCode, msg.StatusDescription)
 	// do not send any subsequent request within this API call
 	api.Key = nil
-	return nil
-}
-
-func (metadata JSONMap) Ttl() (ttl int, err error) {
-	ttl = 0
-	err = nil
-
-	if metaTtl, ok := metadata["defaultBlacklistTtl"]; ok {
-		floatTtl, _ := metaTtl.(float64)
-		ttl = int(floatTtl)
-		if ttl < 0 {
-			ttl = 0
-		}
-		return
-	}
-	err = ErrJsonMetadataDefaultBlacklistTtlMissing
-	return
-}
-
-func (metadata JSONMap) Timestamp() (timestamp int, err error) {
-	timestamp = 0
-	err = nil
-
-	if metaTimestamp, ok := metadata["lastTimestamp"]; ok {
-		floatTimestamp, _ := metaTimestamp.(float64)
-		timestamp = int(floatTimestamp)
-		if timestamp < 0 {
-			timestamp = 0
-		}
-		return
-	}
-	err = ErrJsonMetadataGeneratedatMissing
-	return
-}
-
-func (metadata JSONMap) NextEncodedKey() *string {
-	if metaKey, ok := metadata["nextEncodedKey"]; ok {
-		key, _ := metaKey.(string)
-		if len(key) == 0 {
-			return nil
-		}
-		return &key
-	}
 	return nil
 }
 
